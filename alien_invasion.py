8d71bfda2e7ca57918573f9e6f628809df727d31
rekofke <rekofke.eric@gmail.com>
2025-03-11 22:33:25 -0700

    used groupcollide to eliminate ailens that have been hit and the bullets that hit them

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -77,6 +78,14 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <=0:
                 self.bullets.remove(bullet)
+        # Check for any bullets that havehitaliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+    def _update_aliens(self):
+        """ Update the position of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _create_fleet(self):
         """ Create the fleet of aliens"""
@@ -104,6 +113,19 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
         # self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """ Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """ Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
